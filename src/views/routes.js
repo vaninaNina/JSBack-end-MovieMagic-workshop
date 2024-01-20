c82606f563ab7c8faea0a600cd1a9c96d69b14d8
vaninaNina <kalapsazova.vanina@gmail.com>
2024-01-21 00:10:07 +0200

    adds create movie page

--- src/views/routes.js
+++ src/views/routes.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const homeController = require("../controllers/homeController.js");
 router.use(homeController);
 
+const movieController = require("../controllers/movieController.js");
+router.use(movieController);
+
 router.get("*", (req, res) => {
   res.redirect("/404");
 });
