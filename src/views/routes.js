31744595b3baf41a761020d735644fe975924766
vaninaNina <kalapsazova.vanina@gmail.com>
2024-01-20 23:44:47 +0200

    adds 404 page

--- src/views/routes.js
+++ src/views/routes.js
@@ -3,4 +3,8 @@ const router = require("express").Router();
 const homeController = require("../controllers/homeController.js");
 router.use(homeController);
 
+router.get("*", (req, res) => {
+  res.redirect("/404");
+});
+
 module.exports = router;
