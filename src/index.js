7dfe309a0e0ac086b4c3915eab3fb2d209c35538
vaninaNina <kalapsazova.vanina@gmail.com>
2024-01-20 23:14:22 +0200

    configure of express and handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
+const handlebars = require("express-handlebars");
+const path = require("path");
+const port = 5000;
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.listen(5000, () => console.log("server is listening on port 5000"));
+app.listen(port, () => console.log(`The server is listening on port ${port}`));
