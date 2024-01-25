cc18ea1a2b752968471624e226ec8f9b1c92c3b1
vaninaNina <kalapsazova.vanina@gmail.com>
2024-01-25 22:15:11 +0200

    post action added for cast create

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -4,4 +4,10 @@ router.get("/create", (req, res) => {
   res.render("cast/create");
 });
 
+router.post("/create", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.redirect("/");
+});
+
 module.exports = router;
