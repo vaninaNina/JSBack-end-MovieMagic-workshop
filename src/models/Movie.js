4532452e1ecbaccfd5d0990cddad6702d67c8b85
vaninaNina <kalapsazova.vanina@gmail.com>
2024-01-25 18:36:10 +0200

    movie model created with mongoose

---
+++ src/models/Movie.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const movieScheme = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+    min: 1900,
+    max: 2030,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 1000,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    match: /^https?/,
+  },
+  //   cast:
+});
+
+const Movie = mongoose.model("Model", movieScheme);
+module.exports = Movie;
