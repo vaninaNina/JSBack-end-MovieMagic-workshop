6e52f4516a6f4db0a118a7c8fa97e383ff980c6f
vaninaNina <kalapsazova.vanina@gmail.com>
2024-02-03 15:31:09 +0200

    repeat password validation added

--- src/models/User.js
+++ src/models/User.js
@@ -18,5 +18,11 @@ userSchema.pre("save", async function () {
   this.password = hash;
 });
 
+userSchema.virtual("rePassword").set(function (value) {
+  if (value !== this.password) {
+    throw new mongoose.MongooseError("Password missmatch!");
+  }
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
