fc7a8f251c206dd11edc6b4e4a77cf8d2f1fda8e
vaninaNina <kalapsazova.vanina@gmail.com>
2024-02-03 14:33:05 +0200

    user registered and added at db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
+const authService = require("../service/authService.js");
+
 router.get("/register", (req, res) => {
   res.render("auth/register");
+});
+
+router.post("/register", async (req, res) => {
+  const userData = req.body;
+  await authService.register(userData);
+  res.redirect("/auth/login");
 });
 
 module.exports = router;
